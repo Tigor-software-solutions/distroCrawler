99d3290e51fb642c449fe0897918fe8fd5bd0bf0
236ltAWin10 <developer.tigor@gmail.com>
2021-05-08 01:02:57 +0530

    Crawler first working code.

--- distroCrawler/Program.cs
+++ distroCrawler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NLog;
 using System.Linq;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace distroCrawler
 {
@@ -11,10 +12,10 @@ namespace distroCrawler
         static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            logger.Debug("test");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Distro Crawler Program.");
 
             string url = "https://distrowatch.com/dwres.php?resource=popularity";
+            DateTime dt = DateTime.Now;
             string data = GetWebSiteData(url);
 
             Console.WriteLine("Parsing Data.");
@@ -26,7 +27,11 @@ namespace distroCrawler
             Helper.Utility.WriteCSV(listDistro, fileName);
 
             Console.WriteLine("Date Exported to " + fileName);
-            Console.ReadLine();
+            string message = "Data was extracted in " + (DateTime.Now - dt).Seconds + " secs.";
+            logger.Info(message);
+            Console.WriteLine(message);
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
         }
 
         static string GetWebSiteData(string url)
@@ -51,12 +56,59 @@ namespace distroCrawler
             {
                 string title = node.InnerText;
                 distroTrend.Model.Distro distro = new distroTrend.Model.Distro();
+
+                distro.Code = GetCode(title);
                 distro.Name = title;
 
-                listDistro.Add(distro);
+                if (!listDistro.Exists(x => x.Name == title))
+                    listDistro.Add(distro);
             }
 
             return listDistro;
+        }
+        static string GetCode(string name)
+        {
+            string code;
+            int truncateLength = 8;
+
+            string[] nameArray = name.Split(" ");
+
+            //Default
+            code = name;
+
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            code = rgx.Replace(code, "");
+
+            if (name.Length > truncateLength)
+            {
+                if (nameArray.Length > 1)
+                {
+                    code = string.Empty;
+
+                    foreach (string word in nameArray)
+                    {
+                        int maxSubLength = word.Length;
+                        
+                        if (maxSubLength > truncateLength/2)
+                            maxSubLength = truncateLength/2;
+
+                        code += word.Substring(0, maxSubLength);
+                    }
+                }
+            }   
+
+            code = code.Replace(" ", string.Empty);
+
+            int maxLength = code.Length;
+
+            if (maxLength > truncateLength)
+                maxLength = truncateLength;
+
+            code = code.Substring(0, maxLength);
+
+            logger.Debug("Name=" + name + ", Code=" + code);
+
+            return code;
         }
     }
 }
