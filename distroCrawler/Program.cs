5d7ae99a9131a41e877867b210ac6945dbd70796
236pcWin10 <developer.tigor@gmail.com>
2021-05-22 18:00:51 +0530

    Logic for getting ImageURL and Description.

--- distroCrawler/Program.cs
+++ distroCrawler/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HtmlAgilityPack;
 using NLog;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using HtmlAgilityPack;
 using System.Text.RegularExpressions;
 
 namespace distroCrawler
@@ -18,9 +18,11 @@ namespace distroCrawler
             DateTime dt = DateTime.Now;
             string data = GetWebSiteData(url);
 
-            Console.WriteLine("Parsing Data.");
+            Console.WriteLine("Parsing data started.");
             //logger.Debug(data);
             List<distroTrend.Model.Distro> listDistro = ParseData(data);
+
+            Console.WriteLine("Parsing data completed.");
 
             string fileName = @"C:\temp\distro.csv";
 
@@ -52,20 +54,69 @@ namespace distroCrawler
 
             var nodes = doc.DocumentNode.SelectNodes(@"//td[@class='phr2']/a");
 
+            int counter = 0;
+
             foreach (HtmlNode node in nodes)
             {
+                counter++;
                 string title = node.InnerText;
                 distroTrend.Model.Distro distro = new distroTrend.Model.Distro();
 
+                string[] token = node.OuterHtml.Split(new char[] { '\"' }, StringSplitOptions.RemoveEmptyEntries);
+                string dwCode = string.Empty;
+                if (token.Length > 1)
+                    dwCode = token[1];
+
                 distro.Code = GetCode(title);
                 distro.Name = title;
+                SetSubData(dwCode, name: title, distro);
+
+                logger.Debug("Code=" + distro.Code + ", Name=" + distro.Name + ", Desc=" + distro.Description);
 
                 if (!listDistro.Exists(x => x.Name == title))
                     listDistro.Add(distro);
+
+                string message = counter + ". data Parsed for " + distro.Name;
+                Console.WriteLine(message);
+                if (listDistro.Count > 10)
+                    break;
             }
 
             return listDistro;
         }
+        static void ParseDataSub(string data, string name, distroTrend.Model.Distro distro)
+        {
+            string description = string.Empty;
+            string imageSrc = string.Empty;
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(data);
+
+            var nodesMain = doc.DocumentNode.SelectNodes(@"//td[@class='TablesTitle']/a");
+
+            foreach (HtmlNode node in nodesMain)
+            {
+                if (node.ParentNode.ChildNodes.Count > 10)
+                    description = node.ParentNode.ChildNodes[10].InnerText;
+
+                var nodes = doc.DocumentNode.SelectNodes(@"//img");
+
+                foreach (HtmlNode nodeImg in nodes)
+                {
+                    string search = "title=" + "\"" + name + "\"";
+                    if (nodeImg.OuterHtml.Contains(search))
+                    {
+                        HtmlAttribute img = nodeImg.Attributes.Where(i => i.Name == "src").FirstOrDefault();
+                        distro.ImageURL = img.Value;
+                    }
+                }
+            }
+
+            description = "\"" + description.Replace("\"", "\"\"") + "\"";
+
+            distro.Description = description;
+        }
+
         static string GetCode(string name)
         {
             string code;
@@ -88,14 +139,14 @@ namespace distroCrawler
                     foreach (string word in nameArray)
                     {
                         int maxSubLength = word.Length;
-                        
-                        if (maxSubLength > truncateLength/2)
-                            maxSubLength = truncateLength/2;
+
+                        if (maxSubLength > truncateLength / 2)
+                            maxSubLength = truncateLength / 2;
 
                         code += word.Substring(0, maxSubLength);
                     }
                 }
-            }   
+            }
 
             code = code.Replace(" ", string.Empty);
 
@@ -109,6 +160,15 @@ namespace distroCrawler
             logger.Debug("Name=" + name + ", Code=" + code);
 
             return code;
+        }
+
+        static void SetSubData(string dwCode, string name, distroTrend.Model.Distro distro)
+        {
+            string url = "https://distrowatch.com/table.php?distribution=" + dwCode;
+            DateTime dt = DateTime.Now;
+            string data = GetWebSiteData(url);
+
+            ParseDataSub(data, name, distro);
         }
     }
 }
